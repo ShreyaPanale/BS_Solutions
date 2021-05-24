b215c471fa97c64c3cdb59a2cd011b5287191ead
shreyapanale <shreya.panale@gmail.com>
2021-05-24 22:47:46 +0530

    Adding README

---
+++ Linked Lists/Length of a Linked List/Sol.py
@@ -0,0 +1,13 @@
+class LLNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def solve(self, node):
+        count = 0
+        while(node):
+            count+=1
+            node=node.next
+
+        return count
+        
